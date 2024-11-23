7257d4999ff4be46e92268a8ecae52f35a1385f4
MuhammadAwais989 <officialawais989@gmail.com>
2024-11-23 14:48:59 +0500

    changing in service card

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     <Header/>
     <Routes>
     <Route path='/about' element={<About/>}/>
+    <Route path='/' element={<Service/>}/>
     </Routes>
     </BrowserRouter>
     <HeroSection/>
--- src/Components/Service-Section/Service.jsx
+++ src/Components/Service-Section/Service.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export function Service() {
     
@@ -8,6 +9,10 @@ export function Service() {
             <div className='service-container'>
                 <div className="service-heading">
                     <p>Our Service</p>
+                    <div className="ser-card-cont">
+                        <Link to="/" className='card1'>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
