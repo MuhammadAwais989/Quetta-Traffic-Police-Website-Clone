da9f226433bb64a4b48af6879228ed51e22b8d82
MuhammadAwais989 <officialawais989@gmail.com>
2024-12-08 11:34:09 +0500

    update

--- src/Components/App-Section/AppSection.jsx
+++ src/Components/App-Section/AppSection.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import mobflip from '../../images/mob-flip.ff77cdf3887789d799c5.png'
+import vector from '../../images/vector.115b80534dbc9b6fb4ca.png'
 
 function AppSection() {
   return (
     <div className='app-sec-cont'>
-      <div className='app-react-img'>
 
+      <div className="app-sec-top">
+      <div className='app-react-img'>
+        <img src={mobflip} alt="" />
+        <img src={vector} alt="" />
+      </div>
+
+      <div className='app-sec-heading'>
+        <h1>Download Our App</h1>
+        <span></span>
+      </div>  
+
+      <div className="top-right-img">
+        <img src={vector} alt="" />
+      </div>
       </div>
 
     </div>
